594e44cbd5bb84ab1f284d08c4bf6afaf2933e4a
argy <argy.georgiadis@gmail.com>
2023-05-06 11:20:27 +0300

    ammend parameters

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function generateBoard(width, height) {
    if (!width || !height) {
-      width = randomNumber(8, 15);
-      height = randomNumber(width - 1, 2);
+      width = randomNumber();
+      height = randomNumber();
    }
 
    return Array.from({ length: width }, slot => Array.from({ length: height }, slot => 0));
@@ -12,4 +12,3 @@ function randomNumber(max, min) {
    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-console.log(generateBoard());
